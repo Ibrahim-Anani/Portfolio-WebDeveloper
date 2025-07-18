17b9a11724d2ba42c76b9a0b0ee7cd23a83fb188
ibraheem anani <ibraheemanani234@gmail.com>
2025-07-18 12:55:21 +0300

    Trying to fix Projects Card

--- script.js
+++ script.js
@@ -47,19 +47,3 @@ document.querySelectorAll(".section").forEach(section => {
   });
 });
 
-  document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      const offset = 80; // Adjust this value to match your header height
-      const bodyRect = document.body.getBoundingClientRect().top;
-      const elementRect = target.getBoundingClientRect().top;
-      const elementPosition = elementRect - bodyRect;
-      const offsetPosition = elementPosition - offset;
-
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
-      });
-    });
-  });
