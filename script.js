088b4054152aaa237c358230183592b2af49cead
ibraheem anani <ibraheemanani234@gmail.com>
2025-08-04 20:59:38 +0300

    smooth scroll

--- script.js
+++ script.js
@@ -65,3 +65,12 @@ topBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
